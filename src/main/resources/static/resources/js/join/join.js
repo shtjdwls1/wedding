1705d3160f3f155354f7f417ab53434217aa9873
노성진 <nosj703@naver.com>
2022-10-18 16:35:06 +0900

    10.17 로컬회원가입 유효성검사 등록

--- src/main/resources/static/resources/js/join/join.js
+++ src/main/resources/static/resources/js/join/join.js
@@ -2,7 +2,6 @@
 
 import {SerializeObject} from "@/module/common/serializeObject";
 
-
 $(()=>{
 	new Join();
 })
@@ -18,15 +17,55 @@ export class Join
 			let joinFormData = SerializeObject.run('wedding_joinForm');
 			console.log("wjfd==>{}",joinFormData)
 			console.log(joinFormData)
+			if(checkAll()){
 				axios.post('/data/join',joinFormData).then((result)=> {
 					if(result.data>0){
 						alert("가입성공")
 						location.href = "index";
 					}else{
-						location.href = "join";
+						alert("정보를 올바르게 입력해주세요")
+						location.href="join";
 					}
 				})
+			}
 		})
+		/*
+		 * 이름 입력시 한글만 입력되도록
+		 */
+		$('#inputName').on('keyup',()=>{
+			const regexp = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:"'\\]/g;
+			let v = $('#inputName').val();
+			if (regexp.test(v)) {
+				$('#inputName').val("");
+			}
+		})
+		/*
+		 * 연락처 입력시 숫자만 입력되도록
+		 */
+		$('#tel1').on('keyup',()=>{
+			const regexp = /[^0-9]/gi;
+			let v = $('#tel1').val();
+			if (regexp.test(v)) {
+				$('#tel1').val("");
+			}
+		})
+		$('#tel2').on('keyup',()=>{
+			const regexp = /[^0-9]/gi;
+			let v = $('#tel2').val();
+			if (regexp.test(v)) {
+				$('#tel2').val("");
+			}
+		})
+		$('#tel3').on('keyup',()=>{
+			const regexp = /[^0-9]/gi;
+			let v = $('#tel3').val();
+			if (regexp.test(v)) {
+				$('#tel3').val("");
+			}
+		})
+		/*
+		* 비밀번호 유효성 체크
+		*/
 		$('#inputPw').on('keyup',(e)=>{
 			//비밀번호 대소문자,길이 체크
 			let pass = $(e.currentTarget).val();
@@ -41,7 +80,7 @@ export class Join
 				if(pass.length===0){
 					$('#pwc').addClass("hidden")
 				}else if(pass.length < 8 || pass.length > 16) {
-					message = ":: 최소 8자 이상, 최대 16자 이하 ::";
+					message = "최소 8자 이상, 최대 16자 이하";
 					color = "#A23E48";
 				}
 
@@ -73,16 +112,16 @@ export class Join
 					}
 
 					if(checkPoint >= 3) {
-						message = ":: 보안성이 취약한 비밀번호 ::";
+						message = "보안성이 취약한 비밀번호";
 						color = "#A23E48";
 					} else if(checkPoint === 2) {
-						message = ":: 보안성이 낮은 비밀번호 ::";
+						message = "보안성이 낮은 비밀번호";
 						color = "#FF8C42";
 					} else if(checkPoint === 1) {
-						message = ":: 보안성이 보통인 비밀번호 ::";
+						message = "보안성이 보통인 비밀번호";
 						color = "#FF8C42";
 					} else {
-						message = ":: 보안성이 강력한 비밀번호 ::";
+						message = "보안성이 강력한 비밀번호";
 						color = "#0000CD";
 					}
 				}
@@ -90,6 +129,44 @@ export class Join
 			console.log("ku")
 			$('#pwc').html(message).css('color',color);
 		})
+		/*
+		 * 폼 공백 체크 및 비밀번호 동일 체크
+		 */
+		function checkAll() {
+			if ($('#checkId').val() === "") { //해당 입력값이 없을 경우 같은말: if(!uid.value)
+				$('#checkId').focus(); //focus(): 커서가 깜빡이는 현상, blur(): 커서가 사라지는 현상
+				return false; //return: 반환하다 return false:  아무것도 반환하지 말아라 아래 코드부터 아무것도 진행하지 말것
+			}
+			if ($('#inputPw').val() === "") {
+				$('#inputPw').focus();
+				return false;
+			}
+			if ($('#checkPw').val() === "") {
+				$('#checkPw').focus();
+				return false;
+			}
+			if ($('#inputPw').val() !== $('#checkPw').val()) {
+				$('#checkPw').focus();
+				return false;
+			}
 
+			if ($('#inputName').val() === "") {
+				$('#inputName').focus();
+				return false;
+			}
+			if ($('#tel1').val() === "") {
+				$('#tel1').focus();
+				return false;
+			}
+			if ($('#tel2').val() === "") {
+				$('#tel2').focus();
+				return false;
+			}
+			if ($('#tel3').val() === "") {
+				$('#tel3').focus();
+				return false;
+			}
+			return true;
+		}
 	}
 }
