a09bb4a37f4172e7ad3f612c40de2bd71df49297
노성진 <nosj703@naver.com>
2022-10-25 13:06:35 +0900

    10.25 업체대표이미지 3가지타입 로컬 저장

---
+++ src/main/java/com/smart/project/web/photo/act/PhotoAct.java
@@ -0,0 +1,9 @@
+package com.smart.project.web.photo.act;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class PhotoAct {
+}
--- src/main/resources/static/resources/webpack.config.js
+++ src/main/resources/static/resources/webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
         join: path.join(context, '/js/join/join.js'),
         login: path.join(context,'/js/wedding/login.js'),
         changeCommonInfo: path.join(context,'/js/wedding/changeCommonInfo.js'),
-        changeCommonInfoForm: path.join(context,'/js/wedding/changeCommonInfoForm.js')
+        changeCommonInfoForm: path.join(context,'/js/wedding/changeCommonInfoForm.js'),
+        wedInfoForm:path.join(context,'/js/wedding/photo/photoUpload.js')
     },
     devtool: 'eval',//inline-source-map, eval
     devServer: {
