84804c17ed228e53dbebfd7a03a4901358cccfa4
유재열 <zerosole0@gmail.com>
2022-10-25 10:14:00 +0900

    Revert "10.25 css 제거"
    This reverts commit 74243c656f398dbbd2172daf831fe86284475f0a.

--- src/main/java/com/smart/project/web/counseling/act/MyCounselAct.java
+++ src/main/java/com/smart/project/web/counseling/act/MyCounselAct.java
@@ -3,7 +3,6 @@ package com.smart.project.web.counseling.act;
 import com.smart.project.proc.MyCounselService;
 import com.smart.project.web.counseling.vo.MyCounselUpdateVO;
 import com.smart.project.web.counseling.vo.MyCounselVO;
-import com.smart.project.web.home.vo.MemberVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Slf4j
@@ -25,17 +22,12 @@ public class MyCounselAct {
     final private MyCounselService myCounselService;
 
     @RequestMapping("/myCounsel")
-    public String myCounsel(Model model, HttpServletRequest request) {
+    public String myCounsel(Model model) {
+        int u_idx = 4;
+        log.error("세션1 : {}", u_idx);
 
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            log.error("deleteSessionbefore ==>{}", session.getAttribute("loginSession"));
-        }
-        MemberVO loginMember = (MemberVO) session.getAttribute("loginSession");
-        log.error("세션1 : {}", loginMember.getUIdx());
-
-        List<MyCounselVO> list = myCounselService.myCounsel(loginMember.getUIdx());
-        log.error("결과1 : {}", myCounselService.myCounsel(loginMember.getUIdx()));
+        List<MyCounselVO> list = myCounselService.myCounsel(u_idx);
+        log.error("결과1 : {}", myCounselService.myCounsel(u_idx));
         for (MyCounselVO vo : list) {
             log.error("플래너 : {}", vo.getPGrade());
         }
